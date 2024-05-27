6213ae38a33d6c37c7a37bb8b19fcbf90061a583
Hiroki Chiyoda <contact@arafipro.com>
2024-05-27 12:43:02 +0900

    10_add-highlight

--- app/_components/markdown-preview.tsx
+++ app/_components/markdown-preview.tsx
@@ -1,11 +1,15 @@
 import { md } from "@/constants/md";
+import "highlight.js/styles/github.css";
 import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
 
 export default function MarkdownPreview() {
   return (
     <div>
       <div>{md}</div>
-      <Markdown>{md}</Markdown>
+			<Markdown className="prose" rehypePlugins={[rehypeHighlight]}>
+        {md}
+      </Markdown>
     </div>
   );
 }
